aa61eb6f4bab43c5d7e00e347e089d88fb311e95
JuFML <julianafernandezlima@hotmail.com>
2025-07-26 00:13:41 +0200

    refactor: extract Tailwind classes into variable

--- src/components/StepProgress.tsx
+++ src/components/StepProgress.tsx
@@ -6,6 +6,8 @@ interface StepProgressProps {
 }
 
 const StepProgress = ({ step, steps }: StepProgressProps) => {
+  const stepProgressFillClass = `h-1 absolute top-0 left-0 transition-all duration-300`;
+
   return (
     <div className="flex w-full px-4 justify-center items-start sm:items-center gap-4">
       {steps.map((item, index) => (
@@ -17,7 +19,7 @@ const StepProgress = ({ step, steps }: StepProgressProps) => {
           </div>
           {index < steps.length - 1 && (
             <div className="flex flex-1 h-1 bg-gray-300 mx-2 relative">
-              <div className={`h-1 absolute top-0 left-0 transition-all duration-300 ${step > item.id ? "bg-green-500 w-full" : "bg-gray-300 w-0"}`} />
+              <div className={`${stepProgressFillClass} ${step > item.id ? "bg-green-500 w-full" : "bg-gray-300 w-0"}`} />
             </div>
           )}
         </div>
